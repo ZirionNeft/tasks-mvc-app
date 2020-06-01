e39ca4d711d81eb7538742ed0be9e50cc6408dba
Nikita Kryukov <zirionneft@gmail.com>
2020-06-02 03:04:20 +0700

    env vars for database

--- site/config/general.php
+++ site/config/general.php
@@ -9,10 +9,10 @@ return [
   ],
 
   'database' => [
-    'host'      => 'postgres',
-    'port'      => '5432',
-    'dbname'    => 'app',
-    'user'      => 'app',
+    'host'      => getenv('DB_HOST'),
+    'port'      => getenv('DB_PORT'),
+    'dbname'    => getenv('DB_NAME'),
+    'user'      => getenv('DB_USER'),
     'password'  => getenv('DB_PASSWORD'),
   ]
 ];
