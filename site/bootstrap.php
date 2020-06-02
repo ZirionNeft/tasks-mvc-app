e7e9621d31b8b7f466d1ce1c0f0b89a900a6470a
Nikita Kryukov <zirionneft@gmail.com>
2020-06-02 13:47:44 +0700

    env var fix

--- site/bootstrap.php
+++ site/bootstrap.php
@@ -8,7 +8,9 @@ define('CACHE_DIR', realpath('cache'));
 
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+if (!isset($_ENV['DEV_MODE']) || $_ENV['DEV_MODE'] !== 'production') {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
+}
 
 require_once __DIR__ . "/app/App.php";
